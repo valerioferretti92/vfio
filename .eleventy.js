3a9e76f78a731bb23aa8578737c5824a3fb8533b
Valerio Ferretti <valerio.ferretti92@gmail.com>
2026-01-03 19:03:32 +0100

    Prettified generated HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const prettier = require("prettier");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
@@ -6,6 +8,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/assets/");
   eleventyConfig.addWatchTarget("src/css/");
   eleventyConfig.addWatchTarget("src/js/");
+
+  eleventyConfig.addTransform("prettier", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let prettified = prettier.format(content, {
+        bracketSameLine: true,
+        printWidth: 512,
+        parser: "html",
+        tabWidth: 2,
+      });
+      return prettified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
